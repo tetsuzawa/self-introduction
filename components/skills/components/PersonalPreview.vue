8210c6bbd55cb53415b69fbcdd37cfe0487fc783
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2020-02-06 13:32:07 +0900

    Add skills

---
+++ components/skills/components/PersonalPreview.vue
@@ -0,0 +1,24 @@
+<template>
+    <figure class="image">
+        <img class="personal-preview" :src="image" alt="Phone with personal features preview">
+    </figure>
+</template>
+
+<style lang="scss">
+    .personal-preview {
+    }
+</style>
+
+
+<script>
+    import image from './customisable.png';
+
+    export default {
+        name: 'personal-preview',
+        data() {
+            return {
+                image: image
+            }
+        }
+    }
+</script>
