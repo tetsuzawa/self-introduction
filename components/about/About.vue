a8a3de935f4c88c1d1a1fad0be756c313604b49d
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2020-02-09 11:21:30 +0900

    Remove useless class

--- components/about/About.vue
+++ components/about/About.vue
@@ -3,108 +3,106 @@
     <div class="container">
       <div class="columns is-height-100">
         <div class="column">
-          <!--          <div class="columns is-vcentered is-centered">-->
           <div class="column is-narrow content is-medium">
-            <!--          <div class="content is-medium">-->
             <h3 class="is-size-3">{{ $t('home.about.title') }}</h3>
 
             <table class="table">
-
-              <tr class="is-vcentered is-centered about-row">
-                <td class="is-vcentered">
-                  <div class="about-item is-vcentered">{{ $t('home.about.name_item') }}</div>
+              <tr class="about-row">
+                <td>
+                  <div class="about-item">{{ $t('home.about.name_item') }}</div>
                 </td>
-                <td class="is-vcentered">
-                  <div class="about-content is-vcentered">
+                <td>
+                  <div class="about-content">
                     {{ $t('home.about.primary_name') }} ({{ $t('home.about.secondary_name') }})
                   </div>
                 </td>
               </tr>
 
-              <tr class="is-vcentered is-centered about-row">
-                <td class="is-vcentered">
-                  <div class="about-item is-vcentered">{{ $t('home.about.education_item') }}</div>
+              <tr class="about-row">
+                <td>
+                  <div class="about-item">{{ $t('home.about.education_item') }}</div>
                 </td>
-                <td class="is-vcentered">
-                  <div class="about-content is-vcentered">{{ $t('home.about.education') }}</div>
-                </td>
-              </tr>
-
-              <tr class="is-vcentered is-centered about-row">
-                <td class="is-vcentered">
-                  <div class="about-item is-vcentered">{{ $t('home.about.certification_name') }}</div>
-                </td>
-                <td class="is-vcentered">
-                  <div class="about-content is-vcentered">{{ $t('home.about.toeic') }}</div>
-                  <div class="about-content is-vcentered">{{ $t('home.about.ielts') }}</div>
-                  <div class="about-content is-vcentered">{{ $t('home.about.electric_work') }}</div>
+                <td>
+                  <div class="about-content">{{ $t('home.about.education') }}</div>
                 </td>
               </tr>
 
-              <tr class="is-vcentered is-centered about-row">
-                <td class="is-vcentered">
-                  <div class="about-item is-vcentered">GitHub</div>
+              <tr class="about-row">
+                <td>
+                  <div class="about-item">{{ $t('home.about.certification_name') }}</div>
                 </td>
-                <td class="is-vcentered">
-                  <div class="about-content is-vcentered">
+                <td>
+                  <div class="about-content">{{ $t('home.about.toeic') }}</div>
+                  <div class="about-content">{{ $t('home.about.ielts') }}</div>
+                  <div class="about-content">{{ $t('home.about.electric_work') }}</div>
+                </td>
+              </tr>
+
+              <tr class="about-row">
+                <td>
+                  <div class="about-item">GitHub</div>
+                </td>
+                <td>
+                  <div class="about-content">
                     <a href="https://github.com/tetsuzawa">tetsuzawa</a>
                   </div>
                 </td>
               </tr>
 
 
-              <tr class="is-vcentered is-centered about-row">
-                <td class="is-vcentered">
-                  <div class="about-item is-vcentered">Twitter</div>
+              <tr class="about-row">
+                <td>
+                  <div class="about-item">Twitter</div>
                 </td>
-                <td class="is-vcentered">
-                  <div class="about-content is-vcentered">
+                <td>
+                  <div class="about-content">
                     <a href="https://twitter.com/tetsuzawa_el">tetsuzawa_el</a>
                   </div>
                 </td>
               </tr>
 
-              <tr class="is-vcentered is-centered about-row">
-                <td class="is-vcentered">
-                  <div class="about-item is-vcentered">LinkedIn</div>
+              <tr class="about-row">
+                <td>
+                  <div class="about-item">LinkedIn</div>
                 </td>
-                <td class="is-vcentered">
-                  <div class="about-content is-vcentered">
+                <td>
+                  <div class="about-content">
                     <a href="https://www.linkedin.com/in/tetsu-takizawa">tetsu-takizawa</a>
                   </div>
                 </td>
               </tr>
 
-              <tr class="is-vcentered is-centered about-row">
-                <td class="is-vcentered">
-                  <div class="about-item is-vcentered">Qiita</div>
+              <tr class="about-row">
+                <td>
+                  <div class="about-item">Qiita</div>
                 </td>
-                <td class="is-vcentered">
-                  <div class="about-content is-vcentered">
+                <td>
+                  <div class="about-content">
                     <a href="https://qiita.com/tetsuzawa">tetsuzawa</a>
                   </div>
                 </td>
               </tr>
 
-              <tr class="is-vcentered is-centered about-row">
-                <td class="is-vcentered">
-                  <div class="about-item is-vcentered">Wantedly</div>
+              <tr class="about-row">
+                <td>
+                  <div class="about-item">Wantedly</div>
                 </td>
-                <td class="is-vcentered">
-                  <div class="about-content is-vcentered">
+                <td>
+                  <div class="about-content">
                     <a href="https://www.wantedly.com/users/92008116">瀧澤 哲</a>
                   </div>
                 </td>
               </tr>
-
-              <!--            </div>-->
             </table>
+
           </div>
         </div>
         <div class="column has-padding-top-2x-mobile">
           <div class="columns is-centered is-vcentered is-height-100 is-mobile">
             <div class="column is-narrow">
-<!--              <ob-ghost></ob-ghost>-->
+              <span class="icon">
+                <i class="far fa-address-card fa-lg"></i>
+              </span>
             </div>
           </div>
         </div>
