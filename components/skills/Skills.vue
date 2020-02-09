c05d60bacd1867f07c6e5d6b5e63dfc7ddcdec87
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2020-02-09 11:14:13 +0900

    Change icon to use fa

--- components/skills/Skills.vue
+++ components/skills/Skills.vue
@@ -2,20 +2,18 @@
   <section class="section">
     <div class="container">
       <div class="columns has-reverse-row-order-mobile">
-        <div class="column is-6">
-          <div class="column padding-top-2x-mobile">
-            <div class="columns is-centered is-vcentered is-height-100 is-mobile">
-              <div class="column is-10">
-                <personal-preview/>
-              </div>
-            </div>
+        <div class="column is-6 is-centered is-vcentered section-icon-parent">
+          <div class="column padding-top-2x-mobile has-text-centered section-icon-child">
+            <span class="icon">
+              <i class="fas fa-toolbox section-icon"></i>
+            </span>
           </div>
         </div>
         <div class="column is-6">
           <div class="columns is-centered is-vcentered is-height-100">
             <div class="column is-narrow personal-text content is-medium">
               <h3 class="has-text-shadow">{{ $t('home.skills.title') }}</h3>
-<!--              <h4>{{ $t('home.skills.lang_title') }}</h4>-->
+              <!--              <h4>{{ $t('home.skills.lang_title') }}</h4>-->
               <div class="tags">
                 <span class="tag is-info is-medium">Go</span>
                 <span class="tag is-info is-medium">Python</span>
@@ -25,10 +23,10 @@
                 <span class="tag is-info is-small">Unity C#</span>
                 <span class="tag is-info is-small">HTML5 / CSS3</span>
                 <span class="tag is-info is-small">Shell Script</span>
-<!--                <span class="tag is-primary is-small">Google Apps Script</span>-->
+                <!--                <span class="tag is-primary is-small">Google Apps Script</span>-->
               </div>
 
-<!--              <h4>{{ $t('home.skills.other_title') }}</h4>-->
+              <!--              <h4>{{ $t('home.skills.other_title') }}</h4>-->
               <div class="tags">
                 <span class="tag is-info is-medium">{{ $t('home.skills.web_development') }}</span>
                 <span class="tag is-info is-medium">Git / GitHub</span>
@@ -58,16 +56,25 @@
       max-width: 400px;
     }
   }
+
+  .section-icon {
+    font-size: 200px;
+  }
+
+  .section-icon-parent {
+    position: relative;
+
+    .section-icon-child {
+      position: absolute;
+      top: 40%;
+      left: 50%;
+    }
+  }
 </style>
 
 <script>
 
-    import PersonalPreview from './components/PersonalPreview';
-
     export default {
-        components: {
-            PersonalPreview
-        },
         name: 'skills'
     }
 </script>
